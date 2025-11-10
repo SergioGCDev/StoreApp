5832bf64c65a07d2686f995cc8fb81e3f10be3f2
Sergio 'linco' García <sergiogarciacalero23@gmail.com>
2025-11-10 17:38:03 +0100

    Initial Commit

---
+++ Store/StoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreApp.swift
+//  Store
+//
+//  Created by Sergio García on 10/11/25.
+//
+
+import SwiftUI
+
+@main
+struct StoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
